027b9592a61808563294471acd124cae58fd8498
TheWorkingBee <theworkingbee@hotmail.com>
2023-12-18 16:01:36 +0100

    Solved Day1 part 1

---
+++ 2023/2023/1/Digits.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._1
+{
+    internal class Digits
+    {
+        public string[] input;
+        public List<int> digits = new List<int>();
+        public Digits()
+        {
+            input = File.ReadAllLines("C:\\Users\\yatt\\source\\repos\\AdventOfCode\\2023\\2023\\1\\input.txt");
+        }
+
+        public static char FindDig(char[] val)
+        {
+            foreach (char e in val)
+            {
+                if (char.IsDigit(e)){
+                    return e;
+                }
+            }
+
+            throw new Exception("No Digit");
+        }
+
+        public static string FindDigits(string val)
+        {
+            char[] v = val.ToCharArray();
+            char[] vr = (char[])v.Reverse().ToArray();
+
+            return $"{FindDig(v)}{FindDig(vr)}";
+
+        }
+
+        public void getDigits()
+        {
+            int[] ds = { };
+            foreach (string s in input)
+            {
+                int digs = int.Parse( FindDigits(s));
+                digits.Add(digs);
+            }
+        }
+
+
+    }
+}
--- 2023/2023/Program.cs
+++ 2023/2023/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2023._1;
+
+Digits a = new Digits();
+a.getDigits();
+
+Console.WriteLine(a.digits.Sum());
